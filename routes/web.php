bd39b6133bfd81171b78ff285dc4d1d6d1af0325
David van Oudheusden <doudheusden@novacollege.nl>
2025-04-25 22:57:58 +0200

    added breeze.

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,28 @@
 <?php
 
 use App\Http\Controllers\PredictionController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
-
-require __DIR__ . '/settings.php';
-require __DIR__ . '/auth.php';
 
 Route::get('/', [PredictionController::class, 'create'])->name('home');
 
-
 Route::get('/predictions', [PredictionController::class, 'customerIndex'])->name('predictions.customer');
-Route::get('/predictions/admin', [PredictionController::class, 'index'])->name('predictions.index');
 Route::post('/predictions', [PredictionController::class, 'store'])->name('predictions.store');
 
 Route::put('/predictions/{prediction}/toggle-payment', [PredictionController::class, 'update'])->name('predictions.toggle-payment');
 
 Route::get('/predictions/thankyou', [PredictionController::class, 'thankyou'])->name('predictions.thankyou');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/predictions/admin', [PredictionController::class, 'index'])->name('predictions.index');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
